f3074d87adb17bcbe0167778ee476eb6a5de4e9d
gfransen-sfx <gfransen+sfx@splunk.com>
2025-06-20 15:16:21 +0200

    Main

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { serveStatic } from "https://cdn.jsdelivr.net/npm/@worker-tools/serve-static/+esm";
-
-const serve = serveStatic({ root: "./public" });
-
-export default {
-  fetch(request, env, ctx) {
-    return serve.fetch(request);
-  }
-};
